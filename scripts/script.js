2517dec69d259bd82b751acdc40e7a8b3752a6c2
ButteryToast1991 <omgtoast1991@protonmail.com>
2022-09-15 06:09:48 -0500

    Added logic to toggle styling for completed tasks. TODO: fix logic to properly display styling. Add storage to save tasks. Add logic to delete tasks.

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,29 @@
 //Add tasks to the task list
+const taskList = document.getElementById('taskList');
+
 document.getElementById('addBtn').onclick = function(){
     var task = document.getElementById('toDo').value;
-
+ 
     var taskItem = document.createElement('li');
-    var taskCheck = document.createElement('input').setAttribute('type', 'checkbox');
-    taskItem.innerHTML = '<input type="checkbox">' + task;
+    
+    taskItem.innerHTML = '<input type="checkbox" onclick = isChecked()>' + task;
 
-    document.getElementById('taskList').appendChild(taskItem);
+    taskList.appendChild(taskItem);
+
 }
 
+function isChecked(){
+    const checkbox = document.querySelector('input[type=checkbox]');
+    const taskComplete = document.getElementsByClassName('taskItem');
+
+    checkItem = ((checkbox.classList.contains('checked')) ? true:false);
+    
+    if(checkItem){;
+        taskComplete.classList.remove('complete');
+        console.log(taskComplete.classList);
+
+    }else{
+        taskComplete.classList.add('complete');
+        console.log(taskComplete.classList);
+    }
+}
